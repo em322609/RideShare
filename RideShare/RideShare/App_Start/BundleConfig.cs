9150e35b54f5fee66fc70aefaeac193c03b2a6f3
em322609 <em322609@ohio.edu>
2017-03-02 11:22:00 -0500

    restructured controllers & BundleConfig & web optimization implemented

---
+++ RideShare/RideShare/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace RideShare
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/*.css"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/jquery").Include("~/Scripts/jquery-{version}.js", "~/Scripts/jquery.unobtrusive-ajax.js"));
+        }
+    }
+}
